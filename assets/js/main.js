630df8e9153d59dcb970e2f2fc3c095bfc59c24e
Matheus Martini <matheus.martini@rede.ulbra.br>
2019-06-27 00:19:01 -0300

    alteração de links com jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-function aviso() {
-    alert("linke aqui sua pag externa ");
-}
+$(document).ready(function(){
+    $("#alerta").click(function(evento){
+       alert("link da pagina feito com jquey :)");
+       evento.preventDefault();
+    });
+ });
+$(document).ready(function(){
+    $(".titulo").click(function(evento){
+       alert("fala meu bom, site desenvolvido para o g2 :)");
+       evento.preventDefault();
+    });
+ });
